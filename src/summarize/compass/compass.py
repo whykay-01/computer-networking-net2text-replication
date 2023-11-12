0da4841e7a6fe7bf76306c1fd2b1d439f07b5b9b
Ashley <ajychen26@gmail.com>
2023-11-12 12:32:53 +0800

    update argmax

--- src/summarize/compass/compass.py
+++ src/summarize/compass/compass.py
@@ -11,8 +11,25 @@ OUTPUT:
 A set of specifications S.
 """
 
-def argmax(): # greedy, maximal increase in score
-    return None, None
+def argmax(Q, L, R, t):
+    max_score = -float('inf')
+    best_feature = None
+    best_value = None
+    for q in Q:
+        if len(L) == t:
+            break
+        S = L.union({q})
+        score = 0
+        for r in R:
+            if set(r).issuperset(S):
+                score += 1
+        if score > max_score:
+            max_score = score
+            best_feature = q
+            best_value = S.intersection(set(r))
+        # Compute the score of the candidate feature and update the best feature and value
+        # if the score is higher than the current maximum
+    return best_feature, best_value
 
 def ComPass(R, q, k, t):
     S = set() # The specifications set, set of solutions
@@ -20,8 +37,7 @@ def ComPass(R, q, k, t):
     Q = q # The set of candidate features
     while len(S) < k:
         # Compute the candidate features
-        q, v = argmax(R, q, t) # compute_candidate_features(R, q, t) #TODO: Fix this
-        # Compute the specifications
+        q, v = argmax(Q, L, R, t)
         L = L.union(set(v))
         Q = Q.difference(q)
         R = R.difference(v) # Eliminate routing path that won't satisfy feature values
