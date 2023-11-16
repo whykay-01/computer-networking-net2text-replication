67015876c47538eb49bba4851e9f70ea09333dbb
Yan <100032599+whykay-01@users.noreply.github.com>
2023-11-16 09:54:38 +0800

    feat: modified schema for the DB creation

--- src/db/schema.py
+++ src/db/schema.py
@@ -1,31 +1,30 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from typing import Optional
 
+
 class Base(DeclarativeBase):
-	pass
+    pass
 
 
 class NetworkEntry(Base):
-	__tablename__ = "network"
+    __tablename__ = "network"
 
-	id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-	prefix: Mapped[str]
-	path: Mapped[str]
-	destination: Mapped[str]
-	org: Mapped[str]
-	ingress: Mapped[str]
-	egress: Mapped[str]
-	shortest_path: Mapped[bool]
-	traffic_size: Mapped[int]
-	additional_features: Mapped[Optional[str]]
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    prefix: Mapped[str]
+    path: Mapped[str]
+    destination: Mapped[str]
+    ingress: Mapped[str]
+    egress: Mapped[str]
+    shortest_path: Mapped[bool]
+    traffic_size: Mapped[int]
+    additional_features: Mapped[Optional[str]]
 
-	def __repr__(self) -> str:
-		return f"""Network Entry:
+    def __repr__(self) -> str:
+        return f"""Network Entry:
 			prefix: {self.prefix}
 			path: {self.path}
 			destination: {self.destination}
 			traffic_size: {self.traffic_size}
-			organization: {self.org}
 			ingress: {self.ingress}
 			egress: {self.egress}
 			shortest_path: {self.shortest_path}
