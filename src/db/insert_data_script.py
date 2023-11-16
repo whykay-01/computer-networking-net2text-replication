f7c042643552ed550eba3cc27df86d15ff6f1d0b
Yan <100032599+whykay-01@users.noreply.github.com>
2023-11-16 10:27:49 +0800

    fix: rslvd imports + argument parsing

--- src/db/insert_data_script.py
+++ src/db/insert_data_script.py
@@ -1,18 +1,27 @@
-from sqlalchemy import create_engine
-import pandas as pd
-from load_data import load_example
+# external imports
+import argparse
+import os
+
+# third party imports
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+from sqlalchemy.engine import Connection
+
+# internal imports
+from create import create_db
+from load_data import NDBEntry, load_example
+from schema import NetworkEntry
+from string_utils.string_converter import path_to_str
 
 
-def init_connection():
+def init_connection() -> Connection:
     engine = create_engine("sqlite:///src/db/network.db")
 
     try:
         connection = engine.connect()
-        # cursor = connection.cursor()
         print("Connection established")
 
     except:
-        # cursor = None
         print("Connection failed")
 
     return connection
@@ -38,7 +47,7 @@ def test_connection(connection, table_name: str) -> bool:
         return False
 
 
-def insert_data(connection, data: list[list]):
+def insert_data(db_engine, data: list[NDBEntry]) -> None:
     """
     REF: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_sql.html#r689dfd12abe5-1
     this function takes the connection endpoint and a list of lists of data to insert and inserts it into the database
@@ -50,13 +59,13 @@ def insert_data(connection, data: list[list]):
 
 
 if __name__ == "__main__":
-    connection = init_connection()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "path",
+        help="path to the directory containing the example for the data generation",
+        type=str,
+        default="../../net2text_generator/examples/att_na_100",
+    )
+    parsed_args = parser.parse_args()
 
-    # data = load_example(
-    #     # TODO: figure out what args should I pass. What is the topo and data path?
-    #     topo_path="net2text_generator/examples/att_na_10000",
-    #     data_path="net2text_generator/examples/att_na_10000",
-    # )
-
-    test_connection(connection, "network")
-    terminate_connection(connection)
+    main(parsed_args.path)
