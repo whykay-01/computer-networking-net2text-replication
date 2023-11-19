bb3d0b36856b27e5a3b09466a0f1876508f93ebe
Ashley <ajychen26@gmail.com>
2023-11-19 18:09:42 +0800

    working compass algo

--- src/summarize/compass/compass.py
+++ src/summarize/compass/compass.py
@@ -13,55 +13,37 @@ A set of specifications S.
 
 import sqlite3
 
-# how to score the functions????????
-def argmax(Q, L, R, t):
-    print("inputs:", Q, L, t)
+def score_feature(q, R):
+    # Assuming feature function 'q' returns a boolean if a feature is present in the routing path
+    return sum(1 for r in R if q(r))
+
+def argmax(Q, R):
     max_score = -float('inf')
     best_feature = None
-    best_value = None # what is the feature value
-    for q in Q:
-        if len(L) == t:
-            break
-        S = L.union({q})
-        print("spec set:", S)
-        # Feature scoring function
-        score = 0
-        for r in R:
-            r = r.split(" -> ")
-            if set(r).issuperset(S): # if the path contains the feature, but that's not what it does
-                score += 1
+    for feature in Q:
+        # We assume 'feature' is a callable function that can be applied to the routing paths
+        score = score_feature(feature, R)
         if score > max_score:
             max_score = score
-            best_feature = q
-            best_value = score
-            print("best feature:", best_feature, "best value:", best_value)
-        # Compute the score of the candidate feature and update the best feature and value
-        # if the score is higher than the current maximum
-    print(best_feature, best_value)
-    return best_feature, best_value
+            best_feature = feature
+    return best_feature, max_score
 
-def ComPass(R, q, k, t): # R: set of strings, q: set, k: int, t: int
-    S = set() # The specifications set, set of solutions
-    L = set() # The last computed specification
-    Q = q # The set of candidate features
+def ComPass(R, q, k, t):
+    S = set()  # The specifications set, set of solutions
+    L = set()  # The last computed specification
+    Q = set(q)  # The set of candidate features
+
     while len(S) < k:
-        # Compute the candidate features
-        q, v = argmax(Q, L, R, t)
-        L = L.union({v})
-        Q = Q.difference(q)
-        R = R.difference({v}) # Eliminate routing path that won't satisfy feature values
-        # Add the specifications to the specifications set
-        if len(L) == t:
-            S = S.union(L)
+        best_feature, _ = argmax(Q, R)
+        if best_feature is None:
+            break  # No more features to add
+        L.add(best_feature)
+        if len(L) >= t:
+            S.add(frozenset(L))  # Add as an immutable set
             break
-        
-        while True: # TODO: Add specification for a higher score even though same path
-            L = L.union({v}) # Add the specification with the highest score to the specifications set
-            if len(L) == t:
-                S.add(set(L))
-                break
-            Q = Q.difference(q) # Remove the feature from the candidate features
-        S = S.union(L)
+        Q.remove(best_feature)
+        R = {r for r in R if best_feature(r)}  # Keep only paths where the best feature is present
+
     return S
 
 if __name__ == "__main__":
@@ -70,22 +52,19 @@ if __name__ == "__main__":
     routing_paths = set()
     for row in cur.execute('SELECT path FROM network;'):
         routing_paths.add(row[0])
-    
-    ss = ComPass(routing_paths, {"egress", "ingress","shortest_path", "org"}, 3, 4)
-    print(ss)
+
+    # Example feature functions, replace these with actual logic
+    def egress(path): return "egress" in path
+    def ingress(path): return "ingress" in path
+    def shortest_path(path): return "shortest_path" in path
+    def org(path): return "org" in path
+
+    feature_functions = {egress, ingress, shortest_path, org}
+    output_set = ComPass(routing_paths, feature_functions, 3, 3)
+    spec_set = None
+
+    for spec in output_set:
+        spec_set = [f.__name__ for f in spec]  # List comprehension to get function names
+
+    print(spec_set)
     con.close()
-
-    # Consider R = {(Google, P_i)}^100 i=1. each with weight 1, and k=t=2.
-    # E(Google, P_i) = NY_e and E(Google, P_i) = LA_e otherwise....
-    # feature values should be a key value pair
-
-    # path is a list of STRINGS (even if they look like ints)
-
-"""
-return should look something like this:
-compass_result = [['60', {'egress': "NY"}], 
-                  ['40', {"egress": "Washington"}], 
-                  ['39.6', {"egress": "New York", "shortest_path": True, "waypoint": "Atlanta"}]]
-
-why are the ingress and egress numbers?
-"""
