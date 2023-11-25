9f5a61d7edf09f285fccab66bd3b695bd383d3a4
Yan <100032599+whykay-01@users.noreply.github.com>
2023-11-25 12:04:12 +0800

    chore: changed the table name to the actual one

--- src/parse/NL2SQL.py
+++ src/parse/NL2SQL.py
@@ -1,9 +1,12 @@
-import spacy 
+import spacy
 from spacy.matcher import Matcher
+
 # Load the English language model
 query = "When the ingress is Atlanta, how does the Google's traffic go through?"
 query1 = "How does the Google's traffic go through from New York to Chicago?"
-def parse (query):
+
+
+def parse(query):
     nlp = spacy.load("en_core_web_sm")
     matcher = Matcher(nlp.vocab)
     # Sample English query
@@ -18,30 +21,54 @@ def parse (query):
     shortest_path = False
 
     # Define entity and intent recognition rules
-    #yes or no pattern
+    # yes or no pattern
     yes_or_no_pattern = [{"POS": "AUX"}, {"POS": "PRON"}, {"POS": "VERB"}]
-    #how pattern
+    # how pattern
     how_pattern = [{"LOWER": "how"}, {"LEMMA": {"in": ["be", "will", "would", "do"]}}]
-    #count pattern
+    # count pattern
     count_pattern = [{"LOWER": "how"}, {"LEMMA": "many"}]
-    #data retrieval pattern
+    # data retrieval pattern
     data_retrieval_pattern = [{"LOWER": "what"}, {"LEMMA": "be"}]
     org_pattern = [{"ENT_TYPE": "ORG"}, {"ENT_TYPE": "ORG", "OP": "*"}]
-    egress_pattern1 = [{"TEXT": "to"}, {"ENT_TYPE": "GPE"}, {"ENT_TYPE": "GPE", "OP": "*"}]
-    egress_pattern2 = [{"lower": "egress"}, {"lower": "is"}, {"ENT_TYPE": "GPE"}, {"ENT_TYPE": "GPE", "OP": "*"}]
-    ingress_pattern1 = [{"TEXT": "from"}, {"ENT_TYPE": "GPE"}, {"ENT_TYPE": "GPE", "OP": "*"}]
-    ingress_pattern2 = [{"lower": "ingress"}, {"lower": "is"}, {"ENT_TYPE": "GPE"}, {"ENT_TYPE": "GPE", "OP": "*"}]
-    shortest_path_pattern1 = [{"POS": "ADJ", "LOWER": {"in": ["shortest", "quickest", "fastest"]},
-         "OP": "+"},
-        {"LOWER": "path"}]
-    shortest_path_pattern2 = [{"lower": "shortest"}, {"lower": "path"}, {"lower": "is"}, {"lower": "true"}]
+    egress_pattern1 = [
+        {"TEXT": "to"},
+        {"ENT_TYPE": "GPE"},
+        {"ENT_TYPE": "GPE", "OP": "*"},
+    ]
+    egress_pattern2 = [
+        {"lower": "egress"},
+        {"lower": "is"},
+        {"ENT_TYPE": "GPE"},
+        {"ENT_TYPE": "GPE", "OP": "*"},
+    ]
+    ingress_pattern1 = [
+        {"TEXT": "from"},
+        {"ENT_TYPE": "GPE"},
+        {"ENT_TYPE": "GPE", "OP": "*"},
+    ]
+    ingress_pattern2 = [
+        {"lower": "ingress"},
+        {"lower": "is"},
+        {"ENT_TYPE": "GPE"},
+        {"ENT_TYPE": "GPE", "OP": "*"},
+    ]
+    shortest_path_pattern1 = [
+        {"POS": "ADJ", "LOWER": {"in": ["shortest", "quickest", "fastest"]}, "OP": "+"},
+        {"LOWER": "path"},
+    ]
+    shortest_path_pattern2 = [
+        {"lower": "shortest"},
+        {"lower": "path"},
+        {"lower": "is"},
+        {"lower": "true"},
+    ]
     word_org_pattern = [{"LOWER": "orgnization"}]
     word_egress_pattern = [{"LOWER": "egress"}]
-    word_ingress_pattern = [{"LOWER": 'ingress'}]
+    word_ingress_pattern = [{"LOWER": "ingress"}]
     word_path_pattern = [{"LOWER", "path"}]
     word_traffic_size_pattern = [{"LOWER": "traffic"}, {"LOWER": "size"}]
     word_prefix_pattern = [{"LOWER": "prefix"}]
-    #adding patterns to the matcher
+    # adding patterns to the matcher
     matcher.add("YES_OR_NO_PATTERN", [yes_or_no_pattern])
     matcher.add("HOW_PATTERN", [how_pattern])
     matcher.add("COUNT_PATTERN", [count_pattern])
@@ -49,7 +76,9 @@ def parse (query):
     matcher.add("ORG_PATTERN", [org_pattern])
     matcher.add("EGRESS_PATTERN", [egress_pattern1, egress_pattern2])
     matcher.add("INGRESS_PATTERN", [ingress_pattern1, ingress_pattern2])
-    matcher.add("SHORTEST_PATH_PATTERN", [shortest_path_pattern1, shortest_path_pattern2])
+    matcher.add(
+        "SHORTEST_PATH_PATTERN", [shortest_path_pattern1, shortest_path_pattern2]
+    )
 
     matches = matcher(doc)
     for match_id, start, end in matches:
@@ -62,37 +91,35 @@ def parse (query):
         elif match_id == nlp.vocab.strings["ORG_PATTERN"]:
             org = doc[start:end]
         elif match_id == nlp.vocab.strings["EGRESS_PATTERN"]:
-            egress = doc[end-1]
+            egress = doc[end - 1]
         elif match_id == nlp.vocab.strings["INGRESS_PATTERN"]:
-            ingress = doc[start+1:end]
+            ingress = doc[start + 1 : end]
         elif match_id == nlp.vocab.strings["SHORTEST_PATH_PATTERN"]:
             shortest_path = True
     # Construct the SQL query
 
-
-    def Select_intent (intent):
-    #Question type 1: yes or no questions
+    def Select_intent(intent):
+        # Question type 1: yes or no questions
         if intent == "YES_OR_NO":
-            sql_query = "SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE ENDS AS result FROM paths WHERE"
-    #Question type 2: counting queries
+            sql_query = "SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE ENDS AS result FROM network WHERE"
+        # Question type 2: counting queries
         elif intent == "COUNT":
-            sql_query = "SELECT COUNT(*) FROM paths WHERE"
-    #Question type 3: How questions
+            sql_query = "SELECT COUNT(*) FROM network WHERE"
+        # Question type 3: How questions
         elif intent == "HOW":
-            sql_query = "SELECT * FROM paths WHERE"   
-    #Question type 4 Where questions
+            sql_query = "SELECT * FROM network WHERE"
+        # Question type 4 Where questions
         elif intent == "DATA_RETRIEVAL":
-            sql_query = "SELECT * FROM paths WHERE"
+            sql_query = "SELECT * FROM network WHERE"
         else:
             print("Unable to translate the query.")
         return sql_query
 
-
-    def Select_conditions (sql_query):
+    def Select_conditions(sql_query):
         if ingress:
-            sql_query += f" ingress='{ingress}' AND" 
+            sql_query += f" ingress='{ingress}' AND"
         if egress:
-            sql_query += f" egress='{egress}' AND" 
+            sql_query += f" egress='{egress}' AND"
         if org:
             sql_query += f" org='{org}' AND"
         if shortest_path:
@@ -113,5 +140,7 @@ def parse (query):
             return "Unable to translate the query"
     else:
         return "Unable to translate the query"
+
+
 result = parse(query1)
 print(result)
