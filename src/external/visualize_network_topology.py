10104af418b59343c3e89f58243fd6d2ebf6e963
Yan <100032599+whykay-01@users.noreply.github.com>
2023-11-26 11:56:52 +0800

    feat: added network visualizer

--- src/external/visualize_network_topology.py
+++ src/external/visualize_network_topology.py
@@ -1,6 +1,6 @@
 import networkx as nx
-import matplotlib.pyplot as plt
 import os
+from pyvis.network import Network
 
 root_path = os.getcwd()
 graph_path = os.path.join(
@@ -9,6 +9,14 @@ graph_path = os.path.join(
 # Load the GraphML file
 graph = nx.read_graphml(graph_path)
 
-# Draw the graph
-nx.draw(graph, with_labels=True, font_weight="bold")
-plt.show()
+# # Draw the graph
+# nx.draw(graph, with_labels=True, font_weight="bold")
+# plt.show()
+
+
+# create a network
+net = Network(notebook=False)
+# load the networkx graph
+net.from_nx(graph)
+# show
+net.show("nx.html")
