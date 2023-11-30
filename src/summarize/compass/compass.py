4711631beac9a025786043f392b79ced2c062696
Ashley <ajychen26@gmail.com>
2023-11-30 09:47:54 +0800

    need Uq keep track all feature values

--- src/summarize/compass/compass.py
+++ src/summarize/compass/compass.py
@@ -13,58 +13,79 @@ A set of specifications S.
 
 import sqlite3
 
-def score_feature(q, R):
-    # Assuming feature function 'q' returns a boolean if a feature is present in the routing path
-    return sum(1 for r in R if q(r))
+class RoutingPath:
+    def __init__(self, path, destination, ingress, egress, shortest_path, traffic_size):
+        self.path = path
+        self.destination = destination
+        self.ingress = ingress
+        self.egress = egress
+        self.shortest_path = shortest_path
+        self.traffic_size = traffic_size
+
+class FeatureValue:
+    def __init__(self, q, v):
+        self.q = q
+        self.v = v
+
+def score_feature(v, R):
+    # Use traffic size as weight
+    weight = 0
+    for path in R:
+        weight += path.traffic_size
+    # Compute the score
+    pass #TODO
 
 def argmax(Q, R):
     max_score = -float('inf')
     best_feature = None
-    for feature in Q:
-        # We assume 'feature' is a callable function that can be applied to the routing paths
-        score = score_feature(feature, R)
-        if score > max_score:
-            max_score = score
-            best_feature = feature
-    return best_feature, max_score
+    best_feature_value = None
+    
+    score = score_feature(v, R) #???
+    if score > max_score:
+        max_score = score
+        best_feature = q
+        best_feature_value = v
+    return best_feature, best_feature_value
 
 def ComPass(R, q, k, t):
     S = set()  # The specifications set, set of solutions
     L = set()  # The last computed specification
-    Q = set(q)  # The set of candidate features
+    Q = q  # The set of candidate features
 
     while len(S) < k:
-        best_feature, _ = argmax(Q, R)
-        if best_feature is None:
-            break  # No more features to add
-        L.add(best_feature)
-        if len(L) >= t:
-            S.add(frozenset(L))  # Add as an immutable set
+        q, v = argmax(Q, R)
+        curr_feature = FeatureValue(q, v)
+        L.add(curr_feature)
+        Q.remove(q)
+        
+        if len(L) == t:
             break
-        Q.remove(best_feature)
-        R = {r for r in R if best_feature(r)}  # Keep only paths where the best feature is present
+
+        while L.union({curr_feature}) == L:
+            L.add(curr_feature)
+            if len(L) == t:
+                S.add(L)
+                break
+            Q.remove(curr_feature.q)
+
+        S.add(L)
 
     return S
 
 if __name__ == "__main__":
     con = sqlite3.connect("src/db/network.db")
     cur = con.cursor()
-    routing_paths = set()
-    for row in cur.execute('SELECT path FROM network;'):
-        routing_paths.add(row[0])
+    routing_paths = []
+    for row in cur.execute('SELECT path, destination, traffic_size, ingress, egress, shortest_path FROM network;'):
+        routing_paths.append({
+            'path': row[0], 
+            'destination': row[1],
+            'traffic_size': row[2],
+            'ingress': row[3],
+            'egress': row[4],
+            'shortest_path': row[5]
+        })
 
-    # Example feature functions, replace these with actual logic
-    def egress(path): return "egress" in path
-    def ingress(path): return "ingress" in path
-    def shortest_path(path): return "shortest_path" in path
-    def org(path): return "org" in path
-
-    feature_functions = {egress, ingress, shortest_path, org}
-    output_set = ComPass(routing_paths, feature_functions, 3, 3)
-    spec_set = None
-
-    for spec in output_set:
-        spec_set = [f.__name__ for f in spec]  # List comprehension to get function names
-
-    print(spec_set)
-    con.close()
+    # Example execution
+    specifications = ComPass(routing_paths, {"egress", "ingress", "shortest_path", "organization"}, k=5, t=3)    
+    print(specifications)
